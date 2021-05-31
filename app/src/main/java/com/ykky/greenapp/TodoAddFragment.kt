2a0fee73ddddd8231ea22c18cd6c7d44f20810b7
mimwin <kms105903@naver.com>
2021-05-31 15:43:04 +0900

    TodoData.kt/TodoAddFragment.kt

--- app/src/main/java/com/ykky/greenapp/TodoAddFragment.kt
+++ app/src/main/java/com/ykky/greenapp/TodoAddFragment.kt
@@ -4,56 +4,79 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TodoAddFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TodoAddFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var firebaseauth : FirebaseAuth    // 파이어베이스 인증객체
+    lateinit var databaseref : DatabaseReference    // 실시간 데이터베이스
+    lateinit var todoData : TodoData
+    lateinit var finish : Button
+    lateinit var todoEditText : EditText
+    lateinit var memo : EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo_add, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_todo_add, container, false)
+        finish = view.findViewById<Button>(R.id.finishBtn)
+        todoEditText = view.findViewById(R.id.todoEditText)
+        memo = view.findViewById(R.id.memo)
+
+        init()
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TodoAddFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TodoAddFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun init() {
+        firebaseauth = FirebaseAuth.getInstance()
+        databaseref = FirebaseDatabase.getInstance().getReference("myAppExample")
+
+
+        finish.setOnClickListener {
+
+            val todotext = todoEditText.text.toString()
+            val memo = memo.text.toString()
+
+            val firebaseUser : FirebaseUser? = firebaseauth.currentUser
+            var todoarr : ArrayList<TodoData>
+
+           //기존 투두 가져오기
+            databaseref.child("UserAccount")
+                    .child(firebaseUser?.uid.toString())
+                    .child("todo")
+                    .get().addOnSuccessListener {
+
+                        todoarr = it.value as ArrayList<TodoData>
+
+                        //투두 내용 추가
+                        todoData = TodoData("2021-05-31",todotext,memo,false)
+                        todoarr.add(todoData)
+
+                        //추가한 내용 업데이트
+                        databaseref.child("UserAccount")
+                                .child(firebaseUser?.uid.toString())
+                                .child("todo")
+                                .setValue(todoarr)
+
+                    }.addOnFailureListener {
+                        Toast.makeText(context,"TODO 정보를 불러오는데 실패하였습니다.",Toast.LENGTH_SHORT).show()
+                    }
+
+
+            Toast.makeText(context,"추가 완료",Toast.LENGTH_SHORT).show()
+
+            //todoFragment로 돌아가기
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/ykky/greenapp/TodoData.kt
@@ -0,0 +1,4 @@
+package com.ykky.greenapp
+
+data class TodoData(var data : String, var todo:String, var memo:String, var isChecked : Boolean) {
+}

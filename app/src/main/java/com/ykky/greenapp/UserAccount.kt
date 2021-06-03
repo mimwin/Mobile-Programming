886f30d5c982893db10e16ae054a0fe2caca42e3
mimwin <kms105903@naver.com>
2021-06-03 09:28:56 +0900

    UserAccount.kt Add RegisterDate

--- app/src/main/java/com/ykky/greenapp/UserAccount.kt
+++ app/src/main/java/com/ykky/greenapp/UserAccount.kt
@@ -1,5 +1,5 @@
 package com.ykky.greenapp
 
-data class UserAccount (var emailId : String, var password : String, var token : String, var nickname : String,var todo : ArrayList<TodoData>){
+data class UserAccount (var emailId : String, var password : String, var RegisterDate : String, var nickname : String,var todo : ArrayList<TodoData>){
     //token : Firebase 고유 토큰 (식별자)
 }

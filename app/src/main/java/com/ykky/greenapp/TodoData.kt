3fd1928ae14b4167c7b081299ef372265d69cdcc
Solbee <rnsjthfql@gmail.com>
2021-06-07 23:01:53 +0900

    TodoData default constructor 추가

--- app/src/main/java/com/ykky/greenapp/TodoData.kt
+++ app/src/main/java/com/ykky/greenapp/TodoData.kt
@@ -1,4 +1,5 @@
 package com.ykky.greenapp
 
 data class TodoData(var date : String, var todo:String, var memo:String, var isChecked : Boolean) {
+    constructor():this("2021-01-01", "noinfo","noinfo",false)
 }
